b242af98bee307cd80067c0dad7c7497ba82fe92
Martha Alves <106412221+Martha-Alves@users.noreply.github.com>
2023-02-20 17:05:50 -0300

    subindo ex05

---
+++ 05-evitando-dependencias-externas/evitando-dependencias.js
@@ -0,0 +1,10 @@
+// A variável global
+let fixedValue = 4;
+
+// Altere apenas o código abaixo desta linha
+function incrementer(val) {
+
+return val + 1;
+
+  // Altere apenas o código acima desta linha
+}
