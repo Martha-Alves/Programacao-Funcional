14629c7deb15750f4a96b8b9bdf1c3018ca9a3a9
Martha Alves <106412221+Martha-Alves@users.noreply.github.com>
2023-02-23 00:59:40 -0300

    subindo ex08

--- 08-myMap/forEach.js
+++ 08-myMap/forEach.js
@@ -1,4 +1,17 @@
-08-myMap
+// const socialNetworks = ['youtube', 'twitter', 'instagram', 'facebook']
+
+// //forEach: para cada item do array vamos fazer alguma coisa
+
+// socialNetworks.forEach((item atual, index, array) => {
+//     console.log("olá")
+// })
+
 const socialNetworks = ['youtube', 'twitter', 'instagram', 'facebook']
 
-socialNetworks.forEach()
+const funcc = (socialNetworks, index, array) => {
+    console.log(socialNetworks, index, array)
+}
+
+//forEach: para cada item do array vamos fazer alguma coisa
+
+socialNetworks.forEach(funcc)
