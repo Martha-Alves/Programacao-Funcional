7a1d1d28e57322cf460df0f0500c64878f976b73
Martha Alves <106412221+Martha-Alves@users.noreply.github.com>
2023-02-24 20:41:07 -0300

    subidndo ex24

---
+++ 23-some/some.js
@@ -0,0 +1,8 @@
+function checkPositive(arr) {
+    // Altere apenas o código abaixo desta linha
+  return arr.some(num => num > 0)
+  
+    // Altere apenas o código acima desta linha
+  }
+  
+  checkPositive([1, 2, 3, -4, 5]);
